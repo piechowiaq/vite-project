0e066027623fb2929120f17950ebdcd77c029909
Bartosz Piechowiak <piechowiaq@gmail.com>
2022-12-27 18:23:39 +0100

    Second Commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { useStorage } from "../composables/useStorage.js";
+let food = useStorage('food', 'tacos');
+</script>
+
+<template>
+  <main>
+    <p>
+      What is your favorite food? <input type="text" v-model="food">
+    </p>
+  </main>
+</template>
